a2ddffcd75bc37e39608a75ace1675330ecca4c0
henryhoangduong <henryhoangduong@gmail.com>
2024-08-27 14:19:45 +0700

    create apps

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, APIRouter
-import requests
 import logging
 import os
 import subprocess
+import heroku3
+import re
+import pickle as pkl
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -10,37 +12,63 @@ logging.basicConfig(
 
 router = APIRouter()
 
-@router.get('/')
+
+@router.get("/")
 def root():
-    return {'message':'hello world'}
+    return {"message": "hello world"}
 
-@router.get('/syscall')
+
+@router.get("/syscall")
 def syscall():
-    heroku_api_key = os.getenv('HEROKU_API_KEY')
-    print('heroku_api_key: ', heroku_api_key)
+    heroku_api_key = os.getenv("HEROKU_API_KEY")
+    print("heroku_api_key: ", heroku_api_key)
     try:
-        result = subprocess.run(['heroku', 'whoami'], capture_output=True, text=True)
-        print('result: ', result)
+        result = subprocess.run(["heroku", "whoami"], capture_output=True, text=True)
+        print("result: ", result)
     except Exception as e:
-        print(f'Error login with heroku: {e}')
+        print(f"Error login with heroku: {e}")
 
-@router.get('/syscallwithshell')
+
+@router.get("/syscallwithshell")
 def syscallwithshell():
-    subprocess.run(['sh','heroku.sh'])
+    subprocess.run(["sh", "heroku.sh"])
 
-@router.get('/createapp')
+
+@router.get("/createapp")
 def syscallwithshell():
     try:
-        logging.info('Creating app......')
-        result = subprocess.run(
-            ['heroku', 'create', '--stack', 'container', '--team', 'asol-devops'],
+        logging.info("Creating app......")
+        response = subprocess.run(
+            ["heroku", "create", "--stack", "container", "--team", "asol-devops"],
             capture_output=True,
-            text=True
+            text=True,
         )
-        return {'message':'succesful', 'data':{result}}
+        result = response
+        return {"message": "succesful", "data": {result}}
     except Exception as e:
-        logging.error(f'Error creating apps: {e}')
-    
+        logging.error(f"Error creating apps: {e}")
+        return {'Error':e}
+
+
+@router.get("/appname")
+def get_app_name():
+    logging.info('Getting key....')
+    heroku_api_key = os.getenv("HEROKU_API_KEY")
+    logging.info(f'heroku_api_key:{heroku_api_key}')
+    heroku_conn = heroku3.from_key(heroku_api_key)
+    teams = heroku_conn.apps()
+    result = teams
+    logging.info(f'apps: {result}')
+    return {'data':list(result)}
+
+def clean_ansi_escape_sequences(text):
+    ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
+    return ansi_escape.sub('', text)
+
+def parse_output(text):
+    cleaned_text = clean_ansi_escape_sequences(text)
+    lines = cleaned_text.splitlines()
+    return lines
 
 app = FastAPI()
 app.include_router(router)
